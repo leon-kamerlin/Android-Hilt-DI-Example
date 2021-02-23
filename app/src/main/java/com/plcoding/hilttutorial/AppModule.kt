4185bc7954dd41a789cd5e70bc6fe27846327a98
leonkamerlinn <leonkamerlin@MacBook-Pro.local>
2021-02-23 20:27:58 +0100

    update

--- app/src/main/java/com/plcoding/hilttutorial/AppModule.kt
+++ app/src/main/java/com/plcoding/hilttutorial/AppModule.kt
@@ -3,7 +3,6 @@ package com.plcoding.hilttutorial
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ActivityComponent
 import dagger.hilt.android.components.ApplicationComponent
 import javax.inject.Named
 import javax.inject.Singleton
